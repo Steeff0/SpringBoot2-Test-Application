41df8a3c7df1d94537e8b624e071780ad4a8d87a
Steven Gerritsen <steven.gerritsen@gmail.com>
2020-05-22 19:52:08 +0200

    Fix some problems

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.restservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
