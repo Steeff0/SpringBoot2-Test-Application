56c503c627fa762fa21296b41c8fa7db5a3b76a5
Steven Gerritsen <steven.gerritsen@kadaster.nl>
2021-04-03 16:20:42 +0200

    Update imports

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,11 +1,7 @@
 package com.example.restservice;
 
-import com.example.restservice.pojo.Book;
-import com.example.restservice.repository.BookRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestServiceApplication {
